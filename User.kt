810b261639126784e505fc7f9de93c26dc8ca090
Mike Saravayskiy <maiksaray@gmail.com>
2020-11-06 18:12:49 +0300

    [WIP]Working seeper with self-written auth

--- User.kt
+++ User.kt
@@ -1,3 +1,7 @@
 package common
 
-data class User(val id: Long?, val name: String, val password: String)
+sealed class Role
+object Driver : Role()
+object Admin : Role()
+
+data class User(val id: Long?, val name: String, val password: String, val role: Role?)
