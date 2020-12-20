81bf84a398d6130264240b6fa4cfa3fcba61527f
Mike Saravayskiy <maiksaray@gmail.com>
2020-12-21 00:49:19 +0300

    [WIP] Implemented basic Route functionality HAVE TO TEST IT

--- Route.kt
+++ Route.kt
@@ -4,10 +4,16 @@ import org.joda.time.DateTime
 
 data class Point(val id: Long?, val lat: Double, val lon: Double, val name: String)
 
-data class RoutePoint(val id: Long?, val point: Point, val visited: Boolean)
+data class RoutePoint(val id: Long?, val point: Point, val visited: Boolean, val index:Long)
 
 data class Route(val id: Long?,
                  val driverId: Long?,
                  val routeDate: DateTime,
                  val points: java.util.List<RoutePoint>,
                  val finished: Boolean)
+
+data class CreateRoute(val routeDate: DateTime, val driverId: Long?, val points: java.util.List<Point>)
+
+data class AssignDriver(val driverId: Long)
+
+data class UpdatePoint(val newState: Boolean)
