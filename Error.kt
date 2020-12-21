36af4361eb1a6a5702608aa181c03822cd9b03fd
Mike Saravayskiy <maiksaray@gmail.com>
2020-12-21 21:44:43 +0300

    Added proper logging and communications

--- Error.kt
+++ Error.kt
@@ -13,10 +13,16 @@ data class BadRequest(override val message: String) : Error()
 
 data class InternalServerError(override val message: String) : Error()
 
+data class NotFoundError(override val message: String) : Error()
+
+data class ActionFailed(override val message: String): Error()
+
 object ErrorAdapter {
     val factory = HierarchyTypeAdapterFactory.of(Error::class.java)
             .registerSubtype(InvalidCredentials::class.java)
             .registerSubtype(Unauthorized::class.java)
             .registerSubtype(BadRequest::class.java)
             .registerSubtype(InternalServerError::class.java)
+            .registerSubtype(NotFoundError::class.java)
+            .registerSubtype(ActionFailed::class.java)
 }
